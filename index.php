63834d6f692839344719b9e3344b1bf907827f6b
Anderson Siqueira <contato@andersonls.com.br>
2025-01-08 14:36:58 -0300

    icon-laravel

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comandos Laravel</title>
-    <link rel="shortcut icon" href="https://laravel.com/favicon.ico">
+    <link rel="shortcut icon" href="images/iconlaravel.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 
