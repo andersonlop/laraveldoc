3747a3bdd42ea0361456005a41d53f4b0957be8e
Anderson Siqueira <contato@andersonls.com.br>
2025-01-08 15:40:34 -0300

    .

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
 
         <h1 class="mb-4">Guia Laravel</h1>
 
+        Teste
+
         <section class="mb-5">
             <h2>Pré-requisitos para Criar e Desenvolver um Projeto Laravel</h2>
             <p>Antes de começar a trabalhar com Laravel, certifique-se de ter os seguintes requisitos instalados:</p>
